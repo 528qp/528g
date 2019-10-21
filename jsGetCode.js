7fc8bdababb1aac7fbe650e7882b61a54de09287
528game <56824153+528game@users.noreply.github.com>
2019-10-21 18:40:07 +0800

    Create jsGetCode.js

---
+++ jsGetCode.js
@@ -0,0 +1,15 @@
+OnGetCode = function ( nType )
+{
+    let strCode = "";
+    //public
+    if( nType == 0 )
+    {
+        strCode = "8i5eSjuy/Md0hs80UnO3szTSNbBAG4MPUY+JczWlOUyqPZlyAzguwfe431M+giN8NnWJ2lH4Px9n0z2AN+SJ6/L8PBTQBHiYgcVglsqqpSwCTHos4AqL4MzGzbooiCFD8VwMCY52aPByrtUaB1c63w==";
+    }
+    //Dev
+    else
+    {
+        strCode = "8i5eSjuy/Md0hs80UnO3szTSNbBAG4MPUY+JczWlOUyqPZlyAzguwfe431M+giN8NnWJ2lH4Px9n0z2AN+SJ6/L8PBTQBHiYgcVglsqqpSwCTHos4AqL4MzGzbooiCFD8VwMCY52aPByrtUaB1c63w==";
+    }
+    return  strCode;
+}
